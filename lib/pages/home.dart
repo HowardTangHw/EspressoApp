6cb3ae350bf5d2948dd5d799417014544aaf93c7
Howard Tang <2379034@qq.com>
2024-07-26 17:59:02 +0800

    feat: Nav bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import '../components/progress.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+import 'search.dart';
 import '../util/constants.dart' as constants;
 
 class Home extends StatefulWidget {
@@ -12,167 +13,72 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  @override
-  void initState() {
-    super.initState(); // Call the superclass's initState method
-    //监听输入改变
-    _unameController.addListener(() {
-      print(_unameController.text);
-    });
-  }
+  int _selectedIndex = 0;
 
-  int _counter = 0;
-  final TextEditingController _unameController = TextEditingController();
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+    ),
+    SearchPage(),
+    Text(
+      'Profile',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-        child: Scrollbar(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: constants.Colors.primary,
-                    decoration: TextDecoration.underline,
-                    decorationStyle: TextDecorationStyle.dashed),
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              Wrap(children: [
-                TextButton(
-                    onPressed: () {
-                      context.go("/app_route");
-                    },
-                    child: const Text("open new route")),
-                TextButton(
-                    onPressed: () {
-                      context.go("/box_list");
-                    },
-                    child: const Text("open box list")),
-                TextButton(
-                    onPressed: () {
-                      context.go("/position_example");
-                    },
-                    child: const Text("open Position")),
-                TextButton(
-                    onPressed: () {
-                      context.go("/scollcontroller_example");
-                    },
-                    child: const Text("open scroll")),
-                TextButton(
-                    onPressed: () {
-                      context.go("/custom_scroll_view");
-                    },
-                    child: const Text("open custom scroll")),
-              ]),
-              const ProgressComponent(),
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(" hello world "),
-                      Text(" I am Jack "),
-                    ],
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(" hello world "),
-                      Text(" I am Jack "),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    textDirection: TextDirection.rtl,
-                    children: [
-                      Text(" hello world "),
-                      Text(" I am Jack "),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        " hello world ",
-                        style: TextStyle(fontSize: 30.0),
-                      ),
-                      Text(" I am Jack "),
-                    ],
-                  ),
-                ],
-              ),
-              Container(
-                color: constants.Colors.secondary,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max, //有效，外层Colum高度为整个屏幕
-                    children: <Widget>[
-                      Container(
-                        color: constants.Colors.accent,
-                        child: const Column(
-                          mainAxisSize: MainAxisSize.max, //无效，内层Colum高度为实际高度
-                          children: <Widget>[
-                            Text("hello world "),
-                            Text("I am Jack "),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: constants.Colors.primary.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: constants.Colors.primary,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: const Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: constants.Colors.primary,
+              tabs: const [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
                 ),
-              ),
-              SizedBox(
-                  height: 100.0,
-                  child: Flex(direction: Axis.vertical, children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        height: 40.0,
-                        color: Colors.red,
-                      ),
-                    ),
-                    const Spacer(
-                      flex: 1,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        height: 40.0,
-                        color: Colors.green,
-                      ),
-                    )
-                  ])),
-            ],
+                GButton(
+                  icon: LineIcons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/pages/search.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../components/progress.dart';
+import '../util/constants.dart' as constants;
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+        child: Scrollbar(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: constants.Colors.primary,
+                    decoration: TextDecoration.underline,
+                    decorationStyle: TextDecorationStyle.dashed),
+              ),
+              Wrap(children: [
+                TextButton(
+                    onPressed: () {
+                      context.go("/app_route");
+                    },
+                    child: const Text("open new route")),
+                TextButton(
+                    onPressed: () {
+                      context.go("/box_list");
+                    },
+                    child: const Text("open box list")),
+                TextButton(
+                    onPressed: () {
+                      context.go("/position_example");
+                    },
+                    child: const Text("open Position")),
+                TextButton(
+                    onPressed: () {
+                      context.go("/scollcontroller_example");
+                    },
+                    child: const Text("open scroll")),
+                TextButton(
+                    onPressed: () {
+                      context.go("/custom_scroll_view");
+                    },
+                    child: const Text("open custom scroll")),
+              ]),
+              const ProgressComponent(),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(" hello world "),
+                      Text(" I am Jack "),
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(" hello world "),
+                      Text(" I am Jack "),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    textDirection: TextDirection.rtl,
+                    children: [
+                      Text(" hello world "),
+                      Text(" I am Jack "),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        " hello world ",
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                      Text(" I am Jack "),
+                    ],
+                  ),
+                ],
+              ),
+              Container(
+                color: constants.Colors.secondary,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max, //有效，外层Colum高度为整个屏幕
+                    children: <Widget>[
+                      Container(
+                        color: constants.Colors.accent,
+                        child: const Column(
+                          mainAxisSize: MainAxisSize.max, //无效，内层Colum高度为实际高度
+                          children: <Widget>[
+                            Text("hello world "),
+                            Text("I am Jack "),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                  height: 100.0,
+                  child: Flex(direction: Axis.vertical, children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 40.0,
+                        color: Colors.red,
+                      ),
+                    ),
+                    const Spacer(
+                      flex: 1,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 40.0,
+                        color: Colors.green,
+                      ),
+                    )
+                  ])),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
