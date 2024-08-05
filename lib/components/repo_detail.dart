08f6b4bd2d8d2533157727c49a8dbf3d8e6f986d
Howard Tang <2379034@qq.com>
2024-08-05 12:17:58 +0800

    style: status bar

--- lib/components/repo_detail.dart
+++ lib/components/repo_detail.dart
@@ -7,44 +7,51 @@ class RepoDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: AnnotatedRegion<SystemUiOverlayStyle>(
-            value: const SystemUiOverlayStyle(statusBarColor: Colors.red),
-            child: Scaffold(
-                appBar: AppBar(
-                  title: const Text('detail page'),
-                ),
-                body: SafeArea(
-                  bottom: false,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        title: const Text('Edit'),
-                        leading: const Icon(Icons.edit),
-                        onTap: () => Navigator.of(context).pop(),
-                      ),
-                      ListTile(
-                        title: const Text('Copy'),
-                        leading: const Icon(Icons.content_copy),
-                        onTap: () => Navigator.of(context).pop(),
-                      ),
-                      ListTile(
-                        title: const Text('Cut'),
-                        leading: const Icon(Icons.content_cut),
-                        onTap: () => Navigator.of(context).pop(),
-                      ),
-                      ListTile(
-                        title: const Text('Move'),
-                        leading: const Icon(Icons.folder_open),
-                        onTap: () => Navigator.of(context).pop(),
-                      ),
-                      ListTile(
-                        title: const Text('Delete'),
-                        leading: const Icon(Icons.delete),
-                        onTap: () => Navigator.of(context).pop(),
-                      )
-                    ],
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text('detail page'),
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                // Status bar color
+                statusBarColor: Colors.red,
+
+                // Status bar brightness (optional)
+                statusBarIconBrightness:
+                    Brightness.dark, // For Android (dark icons)
+                statusBarBrightness: Brightness.light, // For iOS (dark icons)
+              ),
+            ),
+            body: SafeArea(
+              bottom: false,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    title: const Text('Edit'),
+                    leading: const Icon(Icons.edit),
+                    onTap: () => Navigator.of(context).pop(),
                   ),
-                ))));
+                  ListTile(
+                    title: const Text('Copy'),
+                    leading: const Icon(Icons.content_copy),
+                    onTap: () => Navigator.of(context).pop(),
+                  ),
+                  ListTile(
+                    title: const Text('Cut'),
+                    leading: const Icon(Icons.content_cut),
+                    onTap: () => Navigator.of(context).pop(),
+                  ),
+                  ListTile(
+                    title: const Text('Move'),
+                    leading: const Icon(Icons.folder_open),
+                    onTap: () => Navigator.of(context).pop(),
+                  ),
+                  ListTile(
+                    title: const Text('Delete'),
+                    leading: const Icon(Icons.delete),
+                    onTap: () => Navigator.of(context).pop(),
+                  )
+                ],
+              ),
+            )));
   }
 }
