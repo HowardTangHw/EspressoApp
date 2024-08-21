c41d30d2a63738b558aefdab34bc223c82bc70f3
louis.lu <louis.lu@prizmcn.com>
2024-08-21 14:09:39 +0800

    styling: seach

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:espresso/util/util.dart';
 import 'package:espresso/components/favorite_list.dart';
+import 'package:espresso/util/constants.dart' as constants;
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -11,6 +12,7 @@ class SearchPage extends StatefulWidget {
 
 class SearchPageState extends State<SearchPage> {
   final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
   String query = '';
   String _printCurrentValue() {
     logger.i(_controller.text);
@@ -21,11 +23,26 @@ class SearchPageState extends State<SearchPage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {}); // 更新状态以刷新界面
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.only(left: 8.0, right: 8.0),
         child: Column(children: [
           TextField(
+            focusNode: _focusNode,
             // autofocus: true,
             controller: _controller,
             onSubmitted: (value) {
@@ -33,14 +50,40 @@ class SearchPageState extends State<SearchPage> {
               _printCurrentValue();
             },
             decoration: InputDecoration(
-              labelText: "Search Key Words",
-              hintText: "",
-              border: InputBorder.none,
-              suffixIcon: GestureDetector(
-                child: const Icon(Icons.search),
-                onTap: () => {_printCurrentValue()},
-              ),
-            ),
+                labelText: "Search Key Words",
+                hintText: "",
+                labelStyle: TextStyle(
+                  color: _focusNode.hasFocus
+                      ? constants.Colors.secondary
+                      : constants.Colors.primary, // 根据焦点状态设置颜色
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30), // 设置圆角边框的半径
+                  borderSide: const BorderSide(color: constants.Colors.primary),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(color: constants.Colors.secondary), // 聚焦时的边框颜色
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: const BorderSide(
+                      color: constants.Colors.primary), // 未选中状态边框颜色
+                ),
+                suffixIcon: Container(
+                  padding: const EdgeInsets.all(8.0), // 背景颜色的内边距
+                  margin: const EdgeInsets.only(right: 20),
+                  decoration: BoxDecoration(
+                    color: _focusNode.hasFocus
+                        ? constants.Colors.secondary
+                        : Colors.transparent, // 背景颜色
+                    shape: BoxShape.circle, // 设置背景为圆形
+                  ),
+                  child: GestureDetector(
+                    child: const Icon(Icons.search),
+                    onTap: () => {_printCurrentValue()},
+                  ),
+                )),
           ),
           Expanded(
             child: query.isNotEmpty ? FavoriteList(query: query) : Container(),
